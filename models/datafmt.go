16838a4e53ae4e0bcf23a34ec896a23e429f746e
Desdemo <1184788560@qq.com>
2019-06-13 15:39:17 +0800

    some test

---
+++ models/datafmt.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"Lw1/json"
+	"time"
+)
+
+type Person1 struct {
+	Id       int64     `json:"id"`
+	Name     string    `json:"name"`
+	Birthday json.Time `json:"birthday"`
+}
+
+func main() {
+	var a json.Time
+	a = json.Time(time.Now())
+
+	p1 := Person1{
+		Id:       1,
+		Name:     "ddd",
+		Birthday: json.Time(time.Now()),
+	}
+	println(json.Marshal(p1))
+	println(a.String())
+}
