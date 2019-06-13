d8c5f2ae2a82a20695d1582bc98592f38eb653a9
Desdemo <1184788560@qq.com>
2019-06-13 15:43:12 +0800

    some test

--- models/swag1.go
+++ models/swag1.go
@@ -23,22 +23,11 @@ import (
 
 func main() {
 
-	r := gin.Default()
-	store := sessions.NewCookieStore([]byte("secret"))
-	r.Use(sessions.Sessions("mysession", store))
-
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
-	v1 := r.Group("/api/v1")
-	{
-		v1.GET("/hello", HandleHello)
-		v1.POST("/login", HandleLogin)
-		v1Auth := r.Use(HandleAuth)
 		{
 			v1Auth.POST("/upload", HandleUpload)
 			v1Auth.GET("/list", HandleList)
 		}
 	}
 
-	r.Run(":8080")
+
 }
